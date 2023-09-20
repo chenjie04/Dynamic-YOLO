85df9d31c4bc2e16cf6b50ac01cc777af0f4adf0
chenjie04 <gxuchenjie04@gmail.com>
2023-09-20 11:18:19 +0800

    调整颈部结构

--- neck.py
+++ neck.py
@@ -5,12 +5,13 @@
 import math
 import torch.nn as nn
 import torch.nn.functional as F
-from mmcv.cnn import build_activation_layer, ConvModule
+from mmcv.cnn import build_activation_layer, ConvModule, build_norm_layer
 from mmengine.model import BaseModule
 from mmdet.registry import MODELS
 from mmdet.utils import ConfigType, OptMultiConfig
 from dcn_v3 import GroupDeformableConvModule
 from mmdet.models.layers import DyReLU
+from mmcv.ops.modulated_deform_conv import ModulatedDeformConv2d
 
 
 @MODELS.register_module()
@@ -197,6 +198,166 @@ class CustomAttnFusionBlock(BaseModule):
         return outs
 
 
+@MODELS.register_module()
+class MultiScaleAttentionFusion_test(BaseModule):
+    """A multi-scale features fusion strategy Base on spatial attention and scale attention.
+
+    Args:
+        in_channels (List[int]): Number of input channels per scale.
+        out_channels (int): Number of output channels (used at each scale).
+        num_fusion_block (int): Number of fusion block.
+        reduction=4 (int): The reduction ratio of channel in compression of query and key.
+        num_heads=2 (int): Number of heads in multi-head attention.
+        mlp_ratio=4 (int): The expansion ratio of channel in feed forward.
+        interpolate_mode = "bilinear" (str): The mode of interprolation for up-sampling and down-sampling.
+        act_cfg=dict(type="Swish") (dict): The config of activation.
+        scale_act_cfg=dict(type="HSigmoid", bias=3.0, divisor=6.0) (dict): The config of activation in scale attenion module.
+        init_cfg=None,
+    """
+
+    def __init__(
+        self,
+        in_channels=[64, 128, 256],
+        out_channel=128,
+        groups=4,
+        mlp_ratio=1.0,
+        drop_rate: float = 0.2,
+        drop_path: float = 0.005,
+        layer_scale: float = 1.0,
+        num_fusion_block=2,
+        norm_cfg: ConfigType = dict(type="GN", num_groups=1, requires_grad=True),
+        act_cfg: ConfigType = dict(type="GELU"),
+        scale_act_cfg=dict(type="HSigmoid", bias=3.0, divisor=6.0),
+        init_cfg: OptMultiConfig = dict(
+            type="Kaiming",
+            layer="Conv2d",
+            a=math.sqrt(5),
+            distribution="uniform",
+            mode="fan_in",
+            nonlinearity="leaky_relu",
+        ),
+    ):
+        super(MultiScaleAttentionFusion_test, self).__init__(init_cfg)
+        self.in_channels = in_channels
+        self.num_levels = len(in_channels)
+        self.num_fusion_block = num_fusion_block
+
+        # 初始融合
+        for level in range(self.num_levels):
+            spatial_attn = nn.Sequential(
+                ModulatedDeformConv2d(
+                    in_channels[level],
+                    out_channel,
+                    3,
+                    padding=1,
+                    deform_groups=in_channels[level] / 32,
+                    bias=False,
+                ),
+                build_norm_layer(norm_cfg, out_channel)[1],
+            )
+            self.add_module(f"init_{level}_spatial_attn", spatial_attn)
+
+            channel_activation = DyReLU(out_channel, act_cfg=(act_cfg, scale_act_cfg))
+            self.add_module(f"init_{level}_channel_activation", channel_activation)
+
+            if level > 0:
+                channel_activation_low = DyReLU(
+                    out_channel, act_cfg=(act_cfg, scale_act_cfg)
+                )
+                self.add_module(
+                    f"init_{level}_channel_activation_low", channel_activation_low
+                )
+
+            if level < self.num_levels - 1:
+                channel_activation_high = DyReLU(
+                    out_channel, act_cfg=(act_cfg, scale_act_cfg)
+                )
+                self.add_module(
+                    f"init_{level}_channel_activation_high", channel_activation_high
+                )
+
+        self.init_scale_attn_module = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            nn.Conv2d(out_channel, 1, 1),
+            # nn.ReLU(inplace=True),
+            build_activation_layer(act_cfg),
+            build_activation_layer(scale_act_cfg),
+        )
+
+        # 后续融合
+        if self.num_fusion_block > 1:
+            follow_fusion_blocks = []
+            for idx in range(self.num_fusion_block):
+                follow_fusion_blocks.append(
+                    CustomAttnFusionBlock(
+                        channels=out_channel,
+                        groups=groups,
+                        mlp_ratio=mlp_ratio,
+                        drop_rate=drop_rate,
+                        drop_path=drop_path,
+                        layer_scale=layer_scale,
+                        norm_cfg=norm_cfg,
+                        act_cfg=act_cfg,
+                        scale_act_cfg=scale_act_cfg,
+                    )
+                )
+
+        self.follow_fusion_block = nn.Sequential(*follow_fusion_blocks)
+
+    def forward(self, inputs):
+        assert len(inputs) == len(self.in_channels)
+
+        # 初始融合
+        init_spatial_outs = []
+        for level in range(len(inputs)):
+            feat = inputs[level]
+            spatial_attn_module = getattr(self, f"init_{level}_spatial_attn")
+            init_spatial_outs.append(spatial_attn_module(feat))
+
+        init_outs = []
+        for level in range(len(init_spatial_outs)):
+            mid_feat = init_spatial_outs[level]
+            channel_activation = getattr(self, f"init_{level}_channel_activation")
+            mid_feat = channel_activation(mid_feat)
+            sum_feat = mid_feat * self.init_scale_attn_module(mid_feat)
+            summed_levels = 1
+            if level > 0:
+                low_feat = F.interpolate(
+                    init_spatial_outs[level - 1],
+                    size=init_spatial_outs[level].shape[-2:],
+                    mode="bilinear",
+                    align_corners=True,
+                )
+                channel_activation_low = getattr(
+                    self, f"init_{level}_channel_activation_low"
+                )
+                low_feat = channel_activation_low(low_feat)
+                sum_feat = sum_feat + low_feat * self.init_scale_attn_module(low_feat)
+                summed_levels += 1
+            if level < len(init_spatial_outs) - 1:
+                # this upsample order is weird, but faster than natural order
+                # https://github.com/microsoft/DynamicHead/issues/25
+                high_feat = F.interpolate(
+                    init_spatial_outs[level + 1],
+                    size=init_spatial_outs[level].shape[-2:],
+                    mode="bilinear",
+                    align_corners=True,
+                )
+                channel_activation_high = getattr(
+                    self, f"init_{level}_channel_activation_high"
+                )
+                high_feat = channel_activation_high(high_feat)
+                sum_feat = sum_feat + high_feat * self.init_scale_attn_module(high_feat)
+                summed_levels += 1
+
+            init_outs.append(sum_feat / summed_levels)
+
+        # 后续融合
+        outputs = self.follow_fusion_block(init_outs)
+
+        return tuple(outputs)
+
+
 # --------------------------------------------------------------------------------------------------------
 # 消融实验
 # --------------------------------------------------------------------------------------------------------
@@ -345,6 +506,7 @@ class BaselineFusionBlock(BaseModule):
             outs.append(conv_fusion_module(sum_feat / summed_levels))
 
         return outs
+
 
 @MODELS.register_module()
 class ChannelAttnNeck(BaseModule):
@@ -520,7 +682,8 @@ class ChannelFusionBlock(BaseModule):
             outs.append(conv_fusion_module(sum_feat / summed_levels))
 
         return outs
-    
+
+
 @MODELS.register_module()
 class ScaleAttnNeck(BaseModule):
     """A multi-scale features fusion strategy Base on spatial attention and scale attention.
@@ -634,7 +797,7 @@ class ScaleFusionBlock(BaseModule):
                 act_cfg=act_cfg,
             )
             self.add_module(f"level_{level}_conv_fusion", convFusion)
-            
+
         self.scale_attn_module = nn.Sequential(
             nn.AdaptiveAvgPool2d(1),
             nn.Conv2d(channels, 1, 1),
@@ -789,7 +952,6 @@ class SpatialFusionBlock(BaseModule):
                 act_cfg=act_cfg,
             )
             self.add_module(f"level_{level}_spatial_attn", spatial_attn)
-            
 
     def forward(self, x):
         outs = []
@@ -822,6 +984,7 @@ class SpatialFusionBlock(BaseModule):
             outs.append(spatial_attn_module(sum_feat / summed_levels))
 
         return outs
+
 
 @MODELS.register_module()
 class ChannelScaleAttnNeck(BaseModule):
@@ -1006,9 +1169,9 @@ class ChannelScaleFusionBlock(BaseModule):
 
         return outs
 
+
 @MODELS.register_module()
 class ChannelSpatialAttnNeck(BaseModule):
-
     def __init__(
         self,
         in_channels=[64, 128, 256],
@@ -1124,7 +1287,6 @@ class ChannelSpatialFusionBlock(BaseModule):
                 self.add_module(
                     f"level_{level}_channel_activation_high", channel_activation_high
                 )
-            
 
     def forward(self, x):
         outs = []
@@ -1156,7 +1318,7 @@ class ChannelSpatialFusionBlock(BaseModule):
                     mode="bilinear",
                     align_corners=True,
                 )
-                channel_activation_high = getattr( 
+                channel_activation_high = getattr(
                     self, f"level_{level}_channel_activation_high"
                 )
                 high_feat = channel_activation_high(high_feat)
@@ -1167,7 +1329,8 @@ class ChannelSpatialFusionBlock(BaseModule):
             outs.append(spatial_attn_module(sum_feat / summed_levels))
 
         return outs
-    
+
+
 @MODELS.register_module()
 class ScaleSpatialAttnNeck(BaseModule):
     def __init__(
@@ -1274,7 +1437,6 @@ class ScaleSpatialFusionBlock(BaseModule):
             build_activation_layer(act_cfg),
             build_activation_layer(scale_act_cfg),
         )
-            
 
     def forward(self, x):
         outs = []
